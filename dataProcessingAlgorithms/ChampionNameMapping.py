b0e0dfe980ce53fca9df9b48927182e03e39fe05
ShiqiLu77 <sikky77@foxmail.com>
2022-12-09 23:35:13 -0500

    feature mapping

---
+++ dataProcessingAlgorithms/ChampionNameMapping.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from tqdm import tqdm
+import os
+
+
+class ChampionNameMapping:
+
+    def __init__(self, namelist, stage):
+        self.namelist = namelist
+        self.stage = stage
+
+    def mapping_all(self,stage):
+        new_feature_columns = []
+        new_feature_values =[]
+
+    
+    def map_counter(self):
+        return None
+
+    def map_attack(self):
+        output =[]
+        
+        return None
+
+    def map_defense(self):
+        return None
+
+    def map_diff_ad(self):
+        return None
+
+    def map_champions_winRate(self):
+        return None
+
+    def map_champions_combo(self):
+        return None
+    
+    def map_champions_control(self):
+        return None
